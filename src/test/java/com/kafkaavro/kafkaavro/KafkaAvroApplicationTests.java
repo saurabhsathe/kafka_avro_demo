b2dacc65d3a4652df1f5efdd55496810ce40fe80
saurabhsathe <sathesaurabh30@gmail.com>
2024-02-28 00:40:01 -0800

    initial commit

---
+++ src/test/java/com/kafkaavro/kafkaavro/KafkaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaavro.kafkaavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
